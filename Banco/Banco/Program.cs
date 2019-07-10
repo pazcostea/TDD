972c5035c0efc254a07e51e34759a5ef75f286a6
pazcostea <pazcostea@gmail.com>
2019-07-10 11:14:21 +0200

    Banco

--- Banco/Banco/Program.cs
+++ Banco/Banco/Program.cs
@@ -6,7 +6,7 @@ namespace banco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); 
         }
     }
 }
