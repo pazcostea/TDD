7c55bd1d1f75d984c19ce064cfbc8763b57fa4d1
pazcostea <pazcostea@gmail.com>
2019-07-10 12:22:17 +0200

    [RED]

---
+++ Banco/Banco/Cuenta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/* 
+ Cree una clase Cuenta que almacena el saldo actual y proporciona los métodos getBalance para obtener el 
+ saldo  y deposit para depositar fondos, withdraw para reitrar fondos y toString para devolver por pantalla
+ el  saldo actual, además de al menos un constructor.
+     */
+
+namespace banco
+{
+    public class Cuenta
+    {
+        public int saldoActual { get; set; }
+
+        //Obtiene el saldo
+        public int getBalance()
+        {
+            throw new Exception("No implementado");
+        }
+        //Deposita fondos
+        public int deposit(int ingresar)
+        {
+            throw new Exception("No implementado");
+        }
+
+        //Retira fondos
+        public int withDraw(int retirar)
+        {
+            throw new Exception("No implementado");
+        }
+
+        //Devuelve por pantalla el saldo actual
+        public override string ToString()
+        {
+            throw new Exception("No implementado");
+        }       
+    }
+}
--- Banco/testBanco/UnitTest1.cs
+++ Banco/testBanco/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using banco;
 
 namespace Tests
 {
@@ -10,9 +11,40 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void comprobarSaldo()
+        {            
+            int cantidad = 100;
+            Cuenta c = new Cuenta();
+            int saldo = c.getBalance();
+            Assert.AreEqual(cantidad, saldo);
+        }
+
+        [Test]
+        public void comprobarDeposito()
         {
-            Assert.Pass();
+            Cuenta c = new Cuenta();
+            c.deposit(100);
+            int saldo = c.getBalance();
+            Assert.AreEqual(c, saldo);
+        }
+
+        [Test]
+        public void comprobarRetirada()
+        {
+            Cuenta c = new Cuenta();
+            c.withDraw(10);
+            int saldo = c.getBalance();
+            Assert.AreEqual(c,saldo);
+        }
+
+        [Test]
+        public void comprobarDevuelve()
+        {
+            Cuenta c = new Cuenta();
+            string saldo;
+            c.saldoActual = 10;
+            saldo = c.ToString();
+            Assert.AreEqual(c, saldo);
         }
     }
 }
